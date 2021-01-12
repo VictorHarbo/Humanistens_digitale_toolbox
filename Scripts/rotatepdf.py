76c4c374251a68366c83f205dbc8976541c9b297
Victor Harbo <vhol@VictorsAir.local>
2021-01-12 14:43:39 +0100

    Added working rotatepdf script

--- Scripts/rotatepdf.py
+++ Scripts/rotatepdf.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-#The top line is used for calling the script from bash
 
-import PyPDF2 #Imports a module that works wonders on PDF's 
+import PyPDF2
 
-pdfIn = open('Original.pdf', 'rb') # exchange the 'original.pdf' with a name of your file 
+pdfIn = open('Gade-udstillingsanalyse2006.pdf', 'rb') # exchange the 'original.pdf' with a name of your file 
 pdfReader = PyPDF2.PdfFileReader(pdfIn)
 pdfWriter = PyPDF2.PdfFileWriter()
 
-for pageNum in range(pdfReader.numPages): #This block of code is applied to all pages in the PDF
+for pageNum in range(pdfReader.numPages):
     page = pdfReader.getPage(pageNum)
-    page.rotateClockwise(-90) #This is where you tell the script how it should rotate the file. -90 turns the pages 90 degrees counterclockwise. 90 turns the PDF 90 degrees clockwise
+    page.rotateClockwise(90)
     pdfWriter.addPage(page)
 
-pdfOut = open('output.pdf', 'wb') #This defines pdfOut as whatever you may have called it
-pdfWriter.write(pdfOut) #Writes the turned PDF to pdfOut
-pdfOut.close() #Closes the PDF-files
+pdfOut = open('Gade-udstillingsanalyse2006rotated.pdf', 'wb')
+pdfWriter.write(pdfOut)
+pdfOut.close()
 pdfIn.close()
