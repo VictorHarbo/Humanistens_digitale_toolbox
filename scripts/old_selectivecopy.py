ac09f32a147813242b2a258d68e6196d832fdec3
Victor Harbo Johnston <vholesen@hotmail.com>
2021-10-12 17:17:58 +0200

    Updated content

--- scripts/old_selectivecopy.py
+++ scripts/old_selectivecopy.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python3
-# Selective copy
+#Selective copy
 #  Walking through a folder tree and searching for files
 #  with a certain file extension('.jpg','.pdf')
 #  and copying from whatever location they are present at to a new folder
 import shutil
 import os
-import argparse
 
-# Makes the program take commandline inputs
-parser = argparse.ArgumentParser(description= "A program, that copies all files in a directory and its subdirectories with the specified ending to a new location on your computer.")
-parser.add_argument("INPUT", help="The path to your input directory.")
-parser.add_argument("OUTPUT", help="The path to where you want your files to be copied to.")
-parser.add_argument("FILETYPE", help="The filetype you want to copy. (PDF, JPG, etc.)")
-parser.add_argument("-b", "--batch", action="store_true", help="")
-args = parser.parse_args()
-
-source = args.INPUT
-newdir = args.OUTPUT
+source ="C:/Users/user/folder/folder/" #Path to where the files are located
+newdir = "C:/Users/user/folder/folder/new_folder/" #Path to where the files should be copied
 
 #This chunk of code makes the new directory where the files are to be transfered, if it already exists it does nothing
 try:
@@ -30,24 +21,24 @@ pdfcount = 0
 #At first, the script crawls trough all files in a directory and all of that directorys underlying subdirectories
 for folders, subfolders, filenames in os.walk(source):
     #Then it is told, not to look into the folder Samlede_tekster
-    if newdir in subfolders:
-        subfolders.remove(newdir)
+    if 'Samlede_tekster' in subfolders:
+        subfolders.remove('Samlede_tekster')
     #Here we tell it to count all of the files that ends in .pdf
     for filename in filenames:
-        if filename.endswith('.'+ args.FILETYPE):
+        if filename.endswith('.pdf'):
             pdfcount += 1
     #finally we tell the computer to copy all files that ends with .pdf into our "newdir"
     for filename in filenames:
-        if filename.endswith('.'+ args.FILETYPE):
+        if filename.endswith('.pdf'):
                 shutil.copy(os.path.join(folders,filename), newdir)
 
 #This new chunk of code counts all the pdf files in "newdir"
 endcount = 0
 for folders, subfolders, filenames in os.walk(newdir):
     for filename in filenames:
-        if filename.endswith('.'+ args.FILETYPE):
+        if filename.endswith('.pdf'):
             endcount += 1
 #At last i want the  script to tell me how many pdf files it found in the directory, that was our source.
 # And then I want i to tell how many pdf files there is in the newly created directory 
-print(f"Found {pdfcount} " + args.FILETYPE + " files in source directory.")
-print(f"Found {endcount} " + args.FILETYPE + " files in the newly created directory.")
+print(f"Found {pdfcount} PDF files in source directory.")
+print(f"Found {endcount} PDF files in the newly created directory.")
