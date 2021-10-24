37b9d2635fbe318fb5f69fe9f8f43bfc558b8bcd
VictorHarbo <vholesen@hotmail.com>
2021-10-24 19:31:25 +0200

    Updated selectivecopy.py to only take uppercase inputs

--- scripts/selectivecopy.py
+++ scripts/selectivecopy.py
@@ -17,9 +17,7 @@ args = parser.parse_args()
 
 source = args.INPUT
 newdir = args.OUTPUT
-
-### TODO: Make the input argument take only uppercase letters.
-### TODO: Make the script parse the input as lowercase and uppercase filenames
+filetype = args.FILETYPE
 
 #This chunk of code makes the new directory where the files are to be transfered, if it already exists it does nothing
 try:
@@ -30,25 +28,30 @@ except:
 #This next chunk does a couple of things.
 # pdfcount = 0 is used to count how many pdf files we have in total in the directory. I am doing this to check that all .pdf files have been copied in the end
 pdfcount = 0
-#At first, the script crawls trough all files in a directory and all of that directorys underlying subdirectories
-for folders, subfolders, filenames in os.walk(source):
-    #Then it is told, not to look into the folder Samlede_tekster
-    if newdir in subfolders:
-        subfolders.remove(newdir)
-    #Here we tell it to count all of the files that ends in .pdf
-    for filename in filenames:
-        if filename.endswith('.'+ args.FILETYPE):
-            pdfcount += 1
-    #finally we tell the computer to copy all files that ends with .pdf into our "newdir"
-    for filename in filenames:
-        if filename.endswith('.'+ args.FILETYPE):
-                shutil.copy(os.path.join(folders,filename), newdir)
+
+if filetype  == filetype.upper():
+    #At first, the script crawls trough all files in a directory and all of that directorys underlying subdirectories
+    for folders, subfolders, filenames in os.walk(source):
+        #Then it is told, not to look into the folder Samlede_tekster
+        if newdir in subfolders:
+            subfolders.remove(newdir)
+        #Here we tell it to count all of the files that ends in .pdf
+        for filename in filenames:
+            if filename.endswith('.'+ args.FILETYPE) or filename.endswith('.'+ args.FILETYPE.lower()):
+                pdfcount += 1
+        #finally we tell the computer to copy all files that ends with .pdf into our "newdir"
+        for filename in filenames:
+            if filename.endswith('.'+ args.FILETYPE) or filename.endswith('.'+ args.FILETYPE.lower()):
+                    shutil.copy(os.path.join(folders,filename), newdir)
+else:
+    print("ERROR: Your FILETYPE-INPUT needs to bo be capitalized")
+    exit()
 
 #This new chunk of code counts all the pdf files in "newdir"
 endcount = 0
 for folders, subfolders, filenames in os.walk(newdir):
     for filename in filenames:
-        if filename.endswith('.'+ args.FILETYPE):
+        if filename.endswith('.'+ args.FILETYPE) or filename.endswith('.'+ args.FILETYPE.lower()):
             endcount += 1
 #At last i want the  script to tell me how many pdf files it found in the directory, that was our source.
 # And then I want i to tell how many pdf files there is in the newly created directory 
